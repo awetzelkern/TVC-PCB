e34576f5682708c458c1ac6fbbb29f77f761aba1
Andrew Kern <awetzelkern@gmail.com>
2026-02-01 19:10:05 -0600

    Added servo control to software

--- TVC Software/src/main.cpp
+++ TVC Software/src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
 #include <pins.h>
 #include <SPI.h>
+#include <Servo.h>
 
 // put function declarations here:
+int xPos = 0; // variable to store the x-axis servo position
+int yPos = 0; // variable to store the y-axis servo position
+
+Servo xServo;
+Servo yServo;
 
 void setup() {
   // put your setup code here, to run once:
+
+  // Servo Setup
+  xServo.attach(TVCX);
+  yServo.attach(TVCY);
+
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600); // baud rate = 9600 bits/s
   pinMode(MEM_CS, OUTPUT); // MCU can set this pin to high or low
@@ -14,11 +25,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-  Serial.println("Hello World");
+
+  // Test Servo Movement
+  for (yPos = 0; yPos <= 180; yPos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      yServo.write(yPos);              // tell servo to go to position in variable 'yPos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  for (yPos = 180; yPos >= 0; yPos -= 1) { // goes from 180 degrees to 0 degrees
+    yServo.write(yPos);              // tell servo to go to position in variable 'yPos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+  for (xPos = 0; xPos <= 180; xPos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    xServo.write(xPos);              // tell servo to go to position in variable 'xPos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (xPos = 180; xPos >= 0; xPos -= 1) { // goes from 180 degrees to 0 degrees
+    xServo.write(xPos);              // tell servo to go to position in variable 'xPos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 
   digitalWrite(MEM_CS, LOW); // begins transaction
   SPI.beginTransaction({10000000, MSBFIRST, SPI_MODE0}); // frequency (10MHz), most significant bit first, default
--- TVC Software/src/pins.h
+++ TVC Software/src/pins.h
@@ -3,9 +3,24 @@
 
 #define MEM_HOLD 0
 #define MEM_CS 1
-
+#define MEM_WRITE_PROTECT 2
+#define BARO_INT 3
+#define BARO_CS 4
+#define RED 5
+#define YLW 6
+#define PY1 7
+#define PY2 8
+#define ORG 9
+#define IMU_CS 10
 #define SPI_MOSI 11
 #define SPI_MISO 12
 #define SPI_SCK 13
-
+#define TVCX 14
+#define TVCY 15
+#define BUZZER_SIG 16
+#define GRN 17
+#define LORA_DIO1 18
+#define LORA_DIO0 19
+#define LORA_CS 20
+#define LORA_RESET 21
 
